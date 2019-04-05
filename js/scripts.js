e718e46e3fdb2c7033d221ccfa8db858bb45c32e
daviekish <davie.kamweru5612@gmail.com>
2019-04-06 01:30:31 +0300

    js final trial on random number,addition of image folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
-// User Interface
-
-// Business logic
+// business logic
+$(document).ready(function () {
+    $("#dice-Roll1").click(() => {
+    var output = Math.floor((Math.random() * 6) + 1);
+    $("#outPut1").text("top number" + " : " + output)
+    });
+});
 
 $(document).ready(function () {
-    $("#dice-roll").click(() => {
-    var output = Math.floor((Math.random() * 6) + 1);
-    $("#outPut1").text("top number: " + output)
+    $("#dice-Roll2").click(() => {
+        var output = Math.floor((Math.random() * 6) + 1);
+        $("#outPut2").text("top number1" + " : " + output)
     });
 });
