9d2bcbac73b50d50579043c62dd3ebb5586ce073
daviekish <davie.kamweru5612@gmail.com>
2019-04-08 00:51:50 +0300

    changes on the javascriptto randomise numbers, spacing the landing page

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 // business logic
 $(document).ready(function () {
-    $("#dice-Roll1").click(() => {
-    var output = Math.floor((Math.random() * 6) + 1);
-    $("#outPut1").text("top number" + " : " + output)
-    });
-});
+$("#dice-Roll1").click(() => {
+ var output1 = Math.floor((Math.random() * 6) + 1);
+ $("#outPut1").text("top number" + " : " + output1);
+ });
+ });
+
 
 $(document).ready(function () {
-    $("#dice-Roll2").click(() => {
-        var output = Math.floor((Math.random() * 6) + 1);
-        $("#outPut2").text("top number1" + " : " + output)
-    });
-});
+  $("#dice-Roll2").click(() => {
+  var output2 = Math.floor((Math.random() * 6) + 1);
+  $("#outPut2").text("top number1" + " : " + output2)
+  });
+ });
